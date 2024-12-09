9c4c5220ec1e323489c04fcc6e01cde21247f26a
jakulej <j.kulej01@gmail.com>
2024-12-09 18:04:59 +0100

    added Comments to preprocess.py

--- preproces.py
+++ preproces.py
@@ -2,6 +2,10 @@ from db import extract_data
 #DATA_TO_ANYLSE = ('citedby_count','author_count')
 DATA_TO_ANYLSE = ('citedby_count','author_count', 'authkeywords', 'fund_sponsor')
 
+#Map each article to dict containing each stat f.eg
+#[citedby_cout: [1,2,4,5]]
+#[author_count: [1,1,2,1]]
+
 
 def preproces(author_id):
     preprocesed_data = {key: [] for key in DATA_TO_ANYLSE}
@@ -12,5 +16,5 @@ def preproces(author_id):
             preprocesed_data[key].append(article[key])
     return preprocesed_data
 
-
-print(preproces("6701511885"))
+#Example
+#print(preproces("6701511885"))
