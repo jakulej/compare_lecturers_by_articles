d20f9301d9b29276cbff7f284a072dfa6d3cc209
Karol Rakicki <rakichuck@gmail.com>
2025-01-12 21:15:29 +0100

    added pca

--- cluster.py
+++ cluster.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 import db
 
-result = db.extract_data("24483671800")
+result = db.extract_data("6701511885")
 
 # Convert result data to a DataFrame
 def convert_to_dataframe(result):
@@ -57,6 +57,24 @@ def visualize_clusters(features, kmeans):
     plt.savefig("clusters.png")  # Save the plot as a PNG file
     plt.close()  # Close the plot to free up memory
 
+from sklearn.decomposition import PCA
+
+# Redukcja wymiarów za pomocą PCA
+def reduce_dimensions(features, n_components=2):
+    pca = PCA(n_components=n_components)
+    reduced_features = pca.fit_transform(features)
+    return reduced_features
+
+# Wizualizacja z wykorzystaniem PCA
+def visualize_clusters_pca(features, kmeans):
+    reduced_features = reduce_dimensions(features, n_components=2)  # Redukcja do 2 wymiarów
+    plt.scatter(reduced_features[:, 0], reduced_features[:, 1], c=kmeans.labels_, cmap='viridis')
+    plt.title('KMeans Clustering')
+    plt.xlabel('Principal Component 1')
+    plt.ylabel('Principal Component 2')
+    plt.savefig("clusters.png")  # Zapisanie wykresu jako PNG
+    plt.close()  # Zamknięcie wykresu po zapisaniu
+
 
 # Process the data
 df_result = convert_to_dataframe(result)
@@ -70,4 +88,4 @@ combined_features_result = combine_features(text_features_result, numerical_feat
 kmeans_result = apply_kmeans(combined_features_result, n_clusters=3)
 
 # Visualize clustering and save to a file
-visualize_clusters(combined_features_result, kmeans_result)
+visualize_clusters_pca(combined_features_result, kmeans_result)
