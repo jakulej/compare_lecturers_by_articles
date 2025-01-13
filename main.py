dab53888ef86c48e555d2ec7565cf3d67b30734a
jakulej <j.kulej01@gmail.com>
2025-01-13 17:24:19 +0000

    Created api call for manova

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from manova import manova
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/manova")
+def get_manova():
+    manova_result = manova()
+    return manova_result
