358aa1cc7bfca8796f22ba25546cea19ef7ce0d5
Wojciech Makuch <wojmak0@gmail.com>
2025-01-27 23:36:51 +0100

    manova

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from manova import manova
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/compare/manova", methods=['POST'])
+@app.route("/manova")
 def get_manova():
-    # data = request.json
-    # lecturer1 = data.get('lecturer1')
-    # lecturer2 = data.get('lecturer2')
-
     manova_result = manova()
     return manova_result
